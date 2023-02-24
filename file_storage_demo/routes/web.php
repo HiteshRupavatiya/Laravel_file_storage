ed83be279f274aa201b6da2dd31f6019b245d8fb
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-24 19:36:43 +0530

    Route added get, post for create post, store post and display all post

--- file_storage_demo/routes/web.php
+++ file_storage_demo/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts',[PostController::class,'index'])->name('post.index');
+
+Route::get('posts/create',[PostController::class,'createPost'])->name('post.create');
+
+Route::post('posts/',[PostController::class,'storePost'])->name('post.store');
