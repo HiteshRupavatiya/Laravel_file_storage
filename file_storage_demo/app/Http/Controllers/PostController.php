40bfa53561961b682c56a61dd5999ae97141821c
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-24 19:34:19 +0530

    PostController added and index, createPost, and storePost method added

--- file_storage_demo/app/Http/Controllers/PostController.php
+++ file_storage_demo/app/Http/Controllers/PostController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpKernel\HttpCache\Store;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = DB::table('posts')->get();
+        return $posts;
+    }
+
+    public function createPost()
+    {
+        return view('create_post');
+    }
+
+    public function storePost(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:10|alpha|unique:posts,title',
+            'body' => 'required|max:255|alpha',
+            'post_image' => 'required|max|image|mimes:jpg,png,jpeg|max:1024',
+            'user_id' => 'required',
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+    }
 }
